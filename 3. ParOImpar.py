0c7aa8b0dfeeaf63d946907510e1607e824d4766
Simon Fernandez-Prada Paz <57737237+simonfernandezprada@users.noreply.github.com>
2020-04-24 20:30:58 -0400

    Add files via upload

---
+++ 3. ParOImpar.py
@@ -0,0 +1,9 @@
+print("Hola! Estas líneas de código quieren saber si el número que ingresas es par o impar.")
+print("Por favor, ingresa un número")
+
+n = int(input())
+
+if n % 2 == 0:
+    print (f"El {n} es un número par")
+else:
+    print (f"El {n} es un número impar")
